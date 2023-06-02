356145018681eba8e218745d45546a0985385bbc
LilianD5 <diazliliandev@gmail.com>
2023-06-02 04:16:56 +0200

    :lipstick: Header + Footer (Non responsive)

--- src/app/footer.js
+++ src/app/footer.js
@@ -1,10 +1,34 @@
 import React from "react"
+import styles from "../../public/css/footer.module.css"
+import Image from "next/image"
+import Link from "next/link"
 
 export default function FooterLayout() {
     return (
         <React.Fragment>
-            <footer>
-                <p>footer</p>
+            <footer className={styles.footer}>
+                <div className={styles.rowLimitSize}>
+                    <div className={styles.top}>
+                        <div className={styles.leftPart}>
+                            <div className={styles.contactWrapper}>
+                                <img src="/assets/phone-icon.svg" alt="Logo téléphone" className={styles.logo} />
+                                <p className={styles.contact}>06 21 63 59 29</p>
+                            </div>
+                            <div className={styles.contactWrapper}>
+                                <img src="/assets/email-icon.svg" alt="Logo e-mail" className={styles.logo} />
+                                <p className={styles.contact}>contact@nathaliediaz-assistante.fr</p>
+                            </div>
+                        </div>
+                        <div className={styles.rightPart}>
+                            <p>assistante administrative et commerciale indépendante</p>
+                            <img className={styles.files} src="/assets/files-icon.svg" alt="Logo de fichiers" />
+                        </div>
+                    </div>
+                    <div className={styles.bottom}>
+                        <Link href="/mentions-legales">mentions légales</Link>
+                        <Link href="/confidentialite">politique de confidentialité</Link>
+                    </div>
+                </div>
             </footer>
         </React.Fragment>
     )
--- src/app/header.js
+++ src/app/header.js
@@ -1,18 +1,19 @@
 import React from "react"
 import Link from "next/link"
 import Image from "next/image"
+import styles from '../../public/css/header.module.css'
 
 export default function HeaderLayout() {
     return (
         <React.Fragment>
-            <header>
-                <menu>
-                    <Image src="/NDA.png" alt="Nda" width="64" height="64" />
-                    <ul>
-                        <Link href="/">accueil</Link>
-                        <Link href="/services">mes services</Link>
-                        <Link href="/avantages">vos avantages</Link>
-                        <Link href="/contact">contact</Link>
+            <header className={styles.headerWrapper}>
+                <menu className={`${styles.menu} ${styles.rowLimitSize}`}>
+                    <Image src="/assets/NDA.png" alt="Nda" width="64" height="64" className={styles.logo} />
+                    <ul className={styles.list}>
+                        <li className={styles.li}><Link href="/">accueil</Link></li>
+                        <li className={styles.li}><Link href="/services">mes services</Link></li>
+                        <li className={styles.li}><Link href="/avantages">vos avantages</Link></li>
+                        <li className={styles.li}><Link href="/contact">contact</Link></li>
                     </ul>
                 </menu>
             </header>
